c69ec4628b3801cc85e1ba4efa00486aaf838f25
Dnyaneshwar Shetkar <dshetkar0101@gmail.com>
2024-05-31 06:46:55 +0530

    Subject Routing Updated

--- student-management-system/src/app/subject/subject-routing.module.ts
+++ student-management-system/src/app/subject/subject-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddSubjectComponent } from './components/add-subject/add-subject.component';
 
 const routes: Routes = [{
-  path
+  path: "",
+  component: AddSubjectComponent
 }];
 
 @NgModule({
