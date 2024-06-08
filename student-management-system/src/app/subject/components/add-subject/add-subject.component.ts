5b8876771894e8d3f312dd59a770fcc7011b4f8b
Dnyaneshwar Shetkar <dshetkar0101@gmail.com>
2024-06-08 09:15:05 +0530

    Date-08-06-2024

--- student-management-system/src/app/subject/components/add-subject/add-subject.component.ts
+++ student-management-system/src/app/subject/components/add-subject/add-subject.component.ts
@@ -1,11 +1,37 @@
-import { Component } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-add-subject',
   templateUrl: './add-subject.component.html',
   styleUrl: './add-subject.component.scss'
 })
-export class AddSubjectComponent {
-  nameControl = new FormControl("John Doe");
+export class AddSubjectComponent implements OnInit {
+  nameControl = new FormControl("John Doe", [
+    Validators.required, 
+    Validators.maxLength(20), 
+    Validators.minLength(4)]);
+
+  subjectForm = new FormGroup({
+    name: new FormControl("", [Validators.required, Validators.maxLength(20), Validators.minLength(10)]),
+    syllabus: new FormControl("", [Validators.required]),
+    status: new FormControl("Active", [Validators.required])
+  });
+
+    ngOnInit(): void {
+      this.nameControl.valueChanges.subscribe(data=>{
+        console.log(data);
+      });
+      this.nameControl.statusChanges.subscribe(data=>{
+        console.log(data);
+      })
+    }
+
+    saveOrUpdateSubject(){
+      if(this.subjectForm.valid){
+        alert("Subject Created Successfully");
+      } else {
+        alert("Subject details are invalid");
+      }
+    }
 }
